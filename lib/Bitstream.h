c825fa5f4f75bb197177ed2a6b05fb6e25c202d3
marcmo <oliver.mueller@gmail.com>
2012-08-02 17:52:53 +0200

    all test running

--- lib/Bitstream.h
+++ lib/Bitstream.h
@@ -6,6 +6,21 @@
 #include <algorithm>
 #include <cassert>
 
+void showbits(int a, int width = 8)
+{
+  int i  , k , mask;
+
+  printf("0b");
+  for( i = width - 1 ; i >= 0 ; i--)
+  {
+     mask = 1 << i;
+     k = a & mask;
+     if( k == 0)
+         printf("0");
+     else
+         printf("1");
+  }
+}
 template<size_t WIDTH>
 struct ones;
 template<>
@@ -41,6 +56,11 @@ struct WidthType<32>
 {
     typedef uint32_t Type;
 };
+template<>
+struct WidthType<64>
+{
+    typedef uint64_t Type;
+};
 
 template<size_t N>
 struct CalculateWidth
@@ -61,6 +81,7 @@ template<> struct typeWidth<uint16_t>
 { enum { value = 16 }; };
 template<> struct typeWidth<uint32_t>
 { enum { value = 32 }; };
+
 class Bitstream
 {
 public:
@@ -147,34 +168,98 @@ private:
     const uint8_t* mData;
 };
 
-class BitstreamWriter
+class BitstreamWriter : Bitstream
 {
 public:
     BitstreamWriter(uint8_t* data, uint32_t length) :
-        mData(data),
-        mLength(length),
-        mOffset(0)
-    {}
+        Bitstream(length),
+        mData(data)
+    {
+        memset(data, 0, length);
+    }
     virtual ~BitstreamWriter ()
     {}
+
+    void debugP(int ci)
+    {
+        printf("mData[%d] changed to ", ci);
+        showbits(mData[ci]);
+        printf("\n");
+    }
 
     template<int N, typename width_type>
     void put(width_type value)
     {
-        printf("putting %d bits, width of type was: %d\n", N, typeWidth<width_type>::value);
+        printf(">>>>>>>>>>>>>>> put ");
+        showbits(value, N);
+        printf(", (N=%d)\n", N);
         int index = (mOffset)/8; // [0,]
         int offsetInByte = (mOffset)%8; // [0,7]
-        int remainingInFirstByte = 8 - offsetInByte; // [1,8]
-        if (N <= remainingInFirstByte)
+        int spaceLeftInFirstByte = 8 - offsetInByte; // [1,8]
+        int bitsForFirstByte = std::min(N,spaceLeftInFirstByte);
+        printf("index: %d, offsetInByte: %d, spaceLeftInFirstByte: %d,bitsForFirstByte:%d\n", index, offsetInByte, spaceLeftInFirstByte, bitsForFirstByte);
+        if (N <= spaceLeftInFirstByte)
         {
-            mData[index] |= value << (remainingInFirstByte - N);
+            printf("%d bits will still fit at data[%d] (%d room)\n", N, index, spaceLeftInFirstByte);
+            mData[index++] |= (value & ones<N>::value) << (spaceLeftInFirstByte - N);
+            debugP(index-1);
+            mOffset += N;
+            printf("mOffset %d -> %d\n", mOffset-N, mOffset);
+        }
+        else
+        {
+            // cover bits in first byte
+            int remainingBits = N - bitsForFirstByte;
+            printf("...... bits to put: %d(offset %d in data[%d])\n", bitsForFirstByte, offsetInByte, index);
+            uint8_t m = mask(offsetInByte, spaceLeftInFirstByte);
+            printf("--- first byte, using mask:");
+            showbits(m);
+            printf(" for first %d bits out of %d (value >> %d = ", bitsForFirstByte, N, N - spaceLeftInFirstByte); 
+            showbits(value >> (N - spaceLeftInFirstByte));
+            printf(") (value: 0x%X)\n", value);
+            mData[index++] |= (value >> (N - spaceLeftInFirstByte)) & m;
+            debugP(index-1);
+            mOffset += spaceLeftInFirstByte;
+            printf("mOffset %d -> %d\n", mOffset-spaceLeftInFirstByte, mOffset);
+            // subsequent bytes
+            while (remainingBits)
+            {
+                printf("...... remaining bits: %d(data[%d])\n", remainingBits, index);
+                if(remainingBits <= 8)
+                {
+                    // process last byte
+                    printf("--- last byte:");
+                    showbits(value & 0xFF);
+                    printf(" shifted: (we only want %d bits) ", remainingBits);
+                    showbits((value & 0xFF) << (8-remainingBits));
+                    printf(" (value=0x%X)\n", value);
+                    mData[index++] |=  (value << (8-remainingBits)) & mask(0,remainingBits);
+                    debugP(index-1);
+                    mOffset += remainingBits;
+                    printf("mOffset %d -> %d\n", mOffset-remainingBits, mOffset);
+                    remainingBits = 0;
+                }
+                else // the full byte is part of the result
+                {
+                    printf("--- subsequent byte %d\n", index);
+                    mData[index++] |=  value >> (remainingBits - 8);
+                    debugP(index-1);
+                    mOffset += 8;
+                    printf("mOffset %d -> %d\n", mOffset-8, mOffset);
+                    remainingBits -= 8;
+                    // res <<= std::min(8,remainingBits); //make room for outstanding bits
+                    // // this is actually equal to 8 but compiler warns about width
+                    // // even though we never get here on an uint_8 type
+                    // res |= mData[index++];
+                    // mOffset += 8;
+                    // remainingBits -= 8;
+                }
+            }
         }
 
     }
 
 private:
     uint8_t* mData;
-    uint32_t mLength;
-    uint32_t mOffset;
 };
 #endif // BITSTREAM_H_
--- lib/BitstreamTest.cpp
+++ lib/BitstreamTest.cpp
@@ -130,10 +130,126 @@ TEST_F(BitstreamTest, getComplexExample)
     EXPECT_EQ(0b1100, bs.get<4>());
 }
 
+// +----------+
+// |1|--- ----|
+// +----------+
 TEST_F(BitstreamTest, putOneBit)
 {
     uint8_t data[1];
     BitstreamWriter bs(data, 1);
-    bs.put<1,uint8_t>(1);
+    bs.put<1>(1);
     EXPECT_EQ(0b10000000, data[0]);
 }
+
+// +-----------+
+// |0|1|00 0000|
+// +-----------+
+TEST_F(BitstreamTest, putOneBitButUseToBigData)
+{
+    uint8_t data[1];
+    BitstreamWriter bs(data, 1);
+    bs.put<1>(0);
+    bs.put<1>(0xFF);
+    EXPECT_EQ(0b01000000, data[0]);
+}
+
+// +----------+
+// |11|10 1000|
+// +----------+
+TEST_F(BitstreamTest, putTwiceInFirstByte)
+{
+    uint8_t data[1];
+    BitstreamWriter bs(data, 1);
+    bs.put<2>(0b11);
+    bs.put<3>(0b101);
+    EXPECT_EQ(0b11101000, data[0]);
+}
+// +---------+---------+
+// |1111 0000 1111|0000|
+// +---------+---------+
+TEST_F(BitstreamTest, putMoreThan8bits)
+{
+    uint8_t data[2];
+    BitstreamWriter bs(data, 2);
+    bs.put<12>(0xF0F);
+    EXPECT_EQ(0b11110000, data[0]);
+    EXPECT_EQ(0b11110000, data[1]);
+}
+// +---------+---------+---------+----------+
+// |0000 0001 0001 0010 0000 0011 1000 00|--|
+// +---------+---------+---------+----------+
+TEST_F(BitstreamTest, putMoreThanIn2bytes)
+{
+    uint8_t data[4]; 
+    BitstreamWriter bs(data, 4);
+    bs.put<30>(0b000000010001001000000011100000);
+    EXPECT_EQ( 0b00000001, data[0]);
+    EXPECT_EQ( 0b00010010, data[1]);
+    EXPECT_EQ( 0b00000011, data[2]);
+    EXPECT_EQ( 0b10000000, data[3]);
+}
+// +----------+---------+---------+----------+---------+
+// |01|10 1101 0101|0010 0100 0011 1|000 1101|1000 0110|
+// +----------+---------+---------+----------+---------+
+TEST_F(BitstreamTest, putComplexExample)
+{
+// 11|100111011|0010010000111|001101|1000110
+    uint8_t data[5]; 
+    BitstreamWriter bs(data, 5);
+    bs.put<2>(0b11);            // 11
+    bs.put<9>(0b100111011);     // 11|10 0111 011
+    bs.put<13>(0b0010010000111);// 11|10 0111 011|0 01001 0000 111
+    bs.put<6>(0b001101);        // 11|10 0111 011|0 01001 0000 111|0 0110 1
+    bs.put<7>(0b1000110);       // 11|10 0111 011|0 01001 0000 111|0 0110 1|100 0110
+    EXPECT_EQ( 0b11100111, data[0]);
+    EXPECT_EQ( 0b01100100, data[1]);
+    EXPECT_EQ( 0b10000111, data[2]);
+    EXPECT_EQ( 0b00110110, data[3]);
+    EXPECT_EQ( 0b00110000, data[4]);
+}
+// +---------+---------+---------+---------+---------+---------+---------+---------+---------+
+// |0110|1101 0101 0010 0100 0011 1000 1101 1000 0110 0101 0010 0100 0011 1000 1101 1000|0110|
+// +---------+---------+---------+---------+---------+---------+---------+---------+---------+
+TEST_F(BitstreamTest, put64Bit)
+{
+    uint8_t data[9]; 
+    BitstreamWriter bs(data, 9);
+    bs.put<4>(0b0110);
+    bs.put<64>(0b1101010100100100001110001101100001100101001001000011100011011000);
+    bs.put<4>(0b0110);
+
+    EXPECT_EQ( 0b01101101, data[0]);
+    EXPECT_EQ( 0b01010010, data[1]);
+    EXPECT_EQ( 0b01000011, data[2]);
+    EXPECT_EQ( 0b10001101, data[3]);
+    EXPECT_EQ( 0b10000110, data[4]);
+    EXPECT_EQ( 0b01010010, data[5]);
+    EXPECT_EQ( 0b01000011, data[6]);
+    EXPECT_EQ( 0b10001101, data[7]);
+    EXPECT_EQ( 0b10000110, data[8]);
+}
+TEST_F(BitstreamTest, putAndGetFun)
+{
+    uint8_t data[20]; 
+    BitstreamReader bsr(data, 20);
+    BitstreamWriter bsw(data, 20);
+    uint8_t a = 0b0110;
+    uint64_t b = 0b1101010100100100001110001101100001100101001001000011100011011000;
+    uint8_t c = 0b10110;
+    uint16_t d = 0b1001010110;
+    uint32_t e = 0b11011011001010110;
+
+    bsw.put<4>(a);
+    bsw.put<64>(b);
+    bsw.put<5>(c);
+    bsw.put<10>(d);
+    bsw.put<17>(e);
+
+    EXPECT_EQ(bsr.get<4>(), a);
+    EXPECT_EQ(bsr.get<64>(), b);
+    EXPECT_EQ(bsr.get<5>(), c);
+    EXPECT_EQ(bsr.get<10>(), d);
+    EXPECT_EQ(bsr.get<17>(), e);
+}
+
+// TEST_F(BitstreamTest, putMoreThanWriterCanTake)
